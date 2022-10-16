42601e22c24738f682f1b66fa8ffb56c97764a34
Vikas <vikbhagat@gmail.com>
2022-10-16 15:49:25 -0500

    Initial Commit

---
+++ test/ContentView.swift
@@ -0,0 +1,131 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by Vikas Bhagat on 4/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var isNight = false
+    
+    var body: some View {
+        ZStack{
+            BackgroundView(topColor: isNight ? .black : .blue,
+                           bottomColor: isNight ? .gray : Color("lightBlue"))
+            VStack {
+                CityTextView(cityName: "Cupertino, CA")
+                MainWeatherStatusView(imageNmae: "cloud.sun.fill",
+                                      temp: 76)
+                HStack(spacing:20){
+                    WeatherDayView(dayOfWeek: "TUE",
+                                   imageName: "cloud.sun.fill",
+                                   temp: 74)
+                   
+                    WeatherDayView(dayOfWeek: "WED",
+                                   imageName: "sun.max.fill",
+                                   temp: 80)
+                    
+                    WeatherDayView(dayOfWeek: "THUR",
+                                   imageName: "wind.snow",
+                                   temp: 40)
+                    
+                    WeatherDayView(dayOfWeek: "FRI",
+                                   imageName: "sunset.fill",
+                                   temp: 77)
+                    
+                    WeatherDayView(dayOfWeek: "SAT",
+                                   imageName: "snow",
+                                   temp: 30)
+                }
+                Spacer()
+                
+                Button{
+                    isNight.toggle()
+                } label: {
+                    WeatherButton(title: "Change Day Time", textColor: Color.blue, backgroundColor: Color.white)
+                 
+                }
+                Spacer()
+            }
+            }
+        }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+struct WeatherDayView: View {
+    var dayOfWeek: String
+    var imageName: String
+    var temp:Int
+    
+    var body: some View {
+        VStack{
+            Text(dayOfWeek)
+                .font(.system(size: 16, weight: .medium, design: .default))
+                .foregroundColor(.white)
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            Text("\(temp)°")
+                .font(.system(size: 28, weight: .medium))
+                .foregroundColor(.white)
+        }
+    }
+}
+
+struct BackgroundView: View {
+    var topColor: Color
+    var bottomColor: Color
+    
+    var body: some View {
+        
+        LinearGradient(gradient: Gradient (colors: [topColor, bottomColor]),
+                       startPoint: .topLeading,
+                       endPoint: .bottomTrailing)
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct CityTextView: View {
+    var cityName: String
+    
+    var body: some View {
+        Text(cityName)
+            .font(.system(size: 32,weight: .medium, design: .default))
+            .foregroundColor(.white)
+            .padding()
+    }
+}
+
+struct MainWeatherStatusView: View{
+    
+    var imageNmae: String
+    var temp: Int
+    
+    var body: some View{
+        VStack(spacing: 10){
+        Image(systemName: imageNmae)
+            .renderingMode(.original)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 180, height: 180)
+        
+        Text("\(temp)°")
+            .font(.system(size: 70, weight: .medium))
+            .foregroundColor(.white)
+        
+    }
+    .padding(.bottom, 40)
+    }
+}
+
---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Vikas Bhagat on 4/10/22.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
